d3e587e005fa191a0df1baeb102f79e0735b913c
ryangholland <rghcode@gmail.com>
2025-05-23 15:03:43 -0400

    feat: create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Header from "./components/Header";
 import Tabs from "./components/Tabs";
 import Today from "./pages/Today";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="bg-gray-900 text-white p-4 md:p-6">
-      <div className="min-h-screen  max-w-4xl mx-auto">
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col">
+      <div className="flex-grow container max-w-4xl mx-auto p-4 md:p-6 ">
         <Header />
         <Tabs />
         <Today />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-center py-4">
+      <p className="text-gray-400 text-sm flex items-center justify-center space-x-2">
+        <span>© 2025 Ryan Holland</span>
+        <a
+          href="https://github.com/ryangholland"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 hover:text-white"
+        >
+          <FontAwesomeIcon icon={faGithub} className="w-4 h-4" />
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
