25b99c6bb0eed943e86001d4acc4a829309a8ac7
ryangholland <rghcode@gmail.com>
2025-05-23 09:26:03 -0400

    feat: create Tabs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+import Tabs from "./components/Tabs";
+
 function App() {
   return (
     <div className="bg-gray-900 text-white p-4 md:p-6">
       <div className="min-h-screen  max-w-4xl mx-auto">
         <Header />
+        <Tabs />
       </div>
     </div>
   );
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,38 @@
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+
+function Tabs() {
+  return (
+    <TabGroup className="p-4">
+      <TabList className="flex border-b border-gray-700 text-2xl gap-8">
+        <Tab
+          className="text-gray-400 border-b-2 border-transparent 
+        hover:text-white hover:border-white 
+        data-selected:text-white data-selected:border-white"
+        >
+          Today
+        </Tab>
+        <Tab
+          className="text-gray-400 border-b-2 border-transparent 
+        hover:text-white hover:border-white
+        data-selected:text-white data-selected:border-white"
+        >
+          This Week
+        </Tab>
+        <Tab
+          className="text-gray-400 border-b-2 border-transparent 
+        hover:text-white hover:border-white
+        data-selected:text-white data-selected:border-white"
+        >
+          Stats
+        </Tab>
+      </TabList>
+      {/* <TabPanels className="mt-4">
+        <TabPanel>Today</TabPanel>
+        <TabPanel>This Week</TabPanel>
+        <TabPanel>Stats</TabPanel>
+      </TabPanels> */}
+    </TabGroup>
+  );
+}
+
+export default Tabs;
