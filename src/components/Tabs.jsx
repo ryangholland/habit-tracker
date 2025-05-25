cf13cb96575335929ef72a4fdc96c2eeb572ea0e
ryangholland <rghcode@gmail.com>
2025-05-25 12:56:41 -0400

    feat: allow user to click tabs to navigate routes

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,36 +1,41 @@
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { Tab, TabGroup, TabList } from "@headlessui/react";
+import { Link, useLocation } from "react-router-dom";
 
 function Tabs() {
+  const location = useLocation();
+
   return (
     <TabGroup className="p-4">
       <TabList className="flex border-b border-gray-700 text-2xl gap-8">
-        <Tab
-          className="text-gray-400 border-b-2 border-transparent pb-2 
-        hover:text-white hover:border-white 
-        data-selected:text-white data-selected:border-white"
-        >
-          Today
-        </Tab>
-        <Tab
-          className="text-gray-400 border-b-2 border-transparent pb-2
+        <Link to="/">
+          <Tab
+            className={`text-gray-400 border-b-2 border-transparent pb-2 
         hover:text-white hover:border-white
-        data-selected:text-white data-selected:border-white"
-        >
-          This Week
-        </Tab>
-        <Tab
-          className="text-gray-400 border-b-2 border-transparent pb-2
+ 
+        ${location.pathname === "/" ? "text-white border-white" : ""}`}
+          >
+            Today
+          </Tab>
+        </Link>
+        <Link to="/this-week">
+          <Tab
+            className={`text-gray-400 border-b-2 border-transparent pb-2
         hover:text-white hover:border-white
-        data-selected:text-white data-selected:border-white"
-        >
-          Stats
-        </Tab>
+        ${location.pathname === "/this-week" ? "text-white border-white" : ""}`}
+          >
+            This Week
+          </Tab>
+        </Link>
+        <Link to="/stats">
+          <Tab
+            className={`text-gray-400 border-b-2 border-transparent pb-2
+        hover:text-white hover:border-white
+        ${location.pathname === "/stats" ? "text-white border-white" : ""}`}
+          >
+            Stats
+          </Tab>
+        </Link>
       </TabList>
-      {/* <TabPanels className="mt-4">
-        <TabPanel>Today</TabPanel>
-        <TabPanel>This Week</TabPanel>
-        <TabPanel>Stats</TabPanel>
-      </TabPanels> */}
     </TabGroup>
   );
 }
