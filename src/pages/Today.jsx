a8f99643309cea34a62b8f2a45019495eac34864
ryangholland <rghcode@gmail.com>
2025-05-27 11:26:47 -0400

    feat: Today's progress bar updates dynamically based on habit completed status

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -3,15 +3,20 @@ import AddHabitForm from "../components/AddHabitForm";
 import ProgressBar from "../components/ProgressBar";
 
 function Today({ habits, setHabits }) {
+  const completedHabits = habits.filter((habit) => habit.completedToday).length;
+  const totalHabits = habits.length;
+  const progress =
+    totalHabits > 0 ? Math.round((completedHabits / totalHabits) * 100) : 0;
+
   return (
     <>
       <h2 className="text-lg">May 23, 2025</h2>
       <p className="italic">
         The journey of a thousand miles begins with a single step.
       </p>
-      <HabitList habits={habits} setHabits={setHabits} />
+      {habits.length > 0 && <HabitList habits={habits} setHabits={setHabits} />}
       <AddHabitForm habits={habits} setHabits={setHabits} />
-      <ProgressBar progress={50} />
+      {habits.length > 0 && <ProgressBar progress={progress} />}
     </>
   );
 }
