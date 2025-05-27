21a9648f6cefb65898ff9828ed027e4261d00941
ryangholland <rghcode@gmail.com>
2025-05-27 10:16:58 -0400

    feat: add habits data to App component, pass down state

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,55 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import { useLocalStorage } from "./hooks/useLocalStorage";
+
 import Today from "./pages/Today";
 import Stats from "./pages/Stats";
 import ThisWeek from "./pages/ThisWeek";
 import Layout from "./layouts/Layout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Today />,
-      },
-      {
-        path: "/this-week",
-        element: <ThisWeek />,
-      },
-      {
-        path: "/stats",
-        element: <Stats />,
-      },
-    ],
-  },
-]);
-
 function App() {
+  const [habits, setHabits] = useLocalStorage("habits", [
+    {
+      id: "uuid",
+      name: "Read 10 pages",
+      completedToday: true,
+      history: {
+        "2025-05-26": true,
+        "2025-05-25": false,
+      },
+    },
+    {
+      id: "uuid2",
+      name: "Work out",
+      completedToday: true,
+      history: {
+        "2025-05-26": true,
+        "2025-05-25": false,
+      },
+    },
+  ]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Today habits={habits} setHabits={setHabits} />,
+        },
+        {
+          path: "/this-week",
+          element: <ThisWeek habits={habits} />,
+        },
+        {
+          path: "/stats",
+          element: <Stats habits={habits} />,
+        },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
