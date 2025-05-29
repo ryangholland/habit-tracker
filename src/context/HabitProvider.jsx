98af40db7ea24cf410801a83a265d54ea088bc5f
ryangholland <rghcode@gmail.com>
2025-05-29 16:37:58 -0400

    feat: on load, check for today's date in habit history

--- src/context/HabitProvider.jsx
+++ src/context/HabitProvider.jsx
@@ -1,7 +1,10 @@
 import { HabitContext } from "./HabitContext";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+import { useToday } from "../hooks/useToday";
 
 export function HabitProvider({ children }) {
+  const { isoDate } = useToday();
+
   const [habits, setHabits] = useLocalStorage("habits", [
     {
       id: 1,
@@ -23,8 +26,26 @@ export function HabitProvider({ children }) {
     },
   ]);
 
+  const updatedHabits = habits.map((habit) => {
+    const hasToday = habit.history?.hasOwnProperty(isoDate);
+    if (!hasToday) {
+      return {
+        ...habit,
+        completedToday: false,
+        history: {
+          ...habit.history,
+          [isoDate]: false,
+        },
+      };
+    }
+    return {
+      ...habit,
+      completedToday: habit.history[isoDate],
+    };
+  });
+
   return (
-    <HabitContext.Provider value={{ habits, setHabits }}>
+    <HabitContext.Provider value={{ habits: updatedHabits, setHabits }}>
       {children}
     </HabitContext.Provider>
   );
--- src/utils/habitUtils.js
+++ src/utils/habitUtils.js
@@ -1,7 +1,17 @@
+import { useToday } from "../hooks/useToday";
+
 export function toggleHabit(habits, id) {
-  return habits.map((habit) =>
-    habit.id === id
-      ? { ...habit, completedToday: !habit.completedToday }
-      : habit
-  );
+  const { isoDate } = useToday();
+
+  return habits.map((habit) => {
+    if (habit.id === id) {
+      const completedToday = !habit.completedToday;
+      const history = {
+        ...habit.history,
+        [isoDate]: completedToday,
+      };
+      return { ...habit, completedToday, history };
+    }
+    return habit;
+  })
 }
