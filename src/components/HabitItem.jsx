36f540ddebe185fa35dcc9d627f6fbfad23be7c9
ryangholland <rghcode@gmail.com>
2025-05-30 08:07:19 -0400

    feat: hide icons on mobile

--- src/components/HabitItem.jsx
+++ src/components/HabitItem.jsx
@@ -1,5 +1,5 @@
 import { Checkbox } from "@headlessui/react";
-import { FaCog, FaTrash } from "react-icons/fa"; // Import icons
+import { FaCog, FaTrash } from "react-icons/fa"; 
 
 function HabitItem({ habit, toggleHabitStatus }) {
   const { id, name, completedToday } = habit;
@@ -7,16 +7,15 @@ function HabitItem({ habit, toggleHabitStatus }) {
   return (
     <div
       className="group flex items-center justify-between space-x-2 gap-2 p-2 rounded-lg border border-gray-700 bg-gray-800 
-        hover:bg-gray-700 hover:border-gray-500  transition duration-200 md:text-xl"
+    hover:bg-gray-700 hover:border-gray-500 hover:cursor-pointer transition duration-200 md:text-lg"
     >
       <div className="flex items-center gap-2">
         <Checkbox
           checked={completedToday}
           onChange={() => toggleHabitStatus(id)}
           className={`group block size-6 rounded border border-gray-400 bg-gray-800 
-            data-checked:bg-gray-400 focus:outline-none hover:cursor-pointer`}
+        data-checked:bg-gray-400 focus:outline-none`}
         >
-          {/* Checkmark icon */}
           <svg
             className="h-4 w-4 text-gray-400 opacity-0 group-data-checked:opacity-100 group-data-checked:text-white"
             viewBox="0 0 14 14"
@@ -33,7 +32,7 @@ function HabitItem({ habit, toggleHabitStatus }) {
         <span className="px-1 text-white">{name}</span>
       </div>
       {/* Icons for Edit and Delete */}
-      <div className="flex items-center gap-2 opacity-0 group-hover:opacity-100 transition duration-200">
+      <div className="hidden md:flex items-center gap-2 opacity-0 md:group-hover:opacity-100 transition duration-200">
         <button
           className="text-gray-400 hover:text-gray-200 hover:cursor-pointer"
           aria-label="Edit Habit"
