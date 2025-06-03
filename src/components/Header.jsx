2146a712a327d5a0700cff0c2a93aaefce647856
ryangholland <rghcode@gmail.com>
2025-06-03 10:19:51 -0400

    feat: change title from 'Habit Tracker' to 'Daily Habit Tracker'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <div className="flex justify-between items-center p-4 ">
-        <h1 className="text-4xl ">Habit Tracker</h1>
+        <h1 className="text-4xl ">Daily Habit Tracker</h1>
         <FaCog size={28} />
       </div>
       <hr />
