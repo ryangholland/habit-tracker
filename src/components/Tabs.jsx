9f1424699a7bed426a183d43ff5106ca0c49ab27
ryangholland <rghcode@gmail.com>
2025-06-06 10:07:16 -0400

    refactor: DRY up NavLink class logic

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,5 +1,14 @@
 import { NavLink } from "react-router-dom";
 
+function getTabClass(isActive) {
+  const base = "pb-2 border-b-2 text-2xl hover:text-white hover:border-white";
+  const active = isActive
+    ? "text-white border-white"
+    : "text-gray-400 border-transparent";
+
+  return `${base} ${active}`;
+}
+
 function Tabs() {
   const baseClasses =
     "pb-2 border-b-2 text-2xl hover:text-white hover:border-white";
@@ -7,41 +16,20 @@ function Tabs() {
   return (
     <div className="p-4">
       <div className="flex border-b border-gray-700 gap-8">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            `${baseClasses} ${
-              isActive
-                ? "text-white border-white"
-                : "text-gray-400 border-transparent"
-            }`
-          }
-        >
+        <NavLink to="/" className={({ isActive }) => getTabClass(isActive)}>
           Today
         </NavLink>
-        
+
         <NavLink
           to="/stats"
-          className={({ isActive }) =>
-            `${baseClasses} ${
-              isActive
-                ? "text-white border-white"
-                : "text-gray-400 border-transparent"
-            }`
-          }
+          className={({ isActive }) => getTabClass(isActive)}
         >
           Stats
         </NavLink>
 
         <NavLink
           to="/settings"
-          className={({ isActive }) =>
-            `${baseClasses} ${
-              isActive
-                ? "text-white border-white"
-                : "text-gray-400 border-transparent"
-            }`
-          }
+          className={({ isActive }) => getTabClass(isActive)}
         >
           Settings
         </NavLink>
