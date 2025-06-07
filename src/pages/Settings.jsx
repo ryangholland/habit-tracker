15916f08bd775446564db191213c3fece871d858
ryangholland <rghcode@gmail.com>
2025-06-07 08:40:57 -0400

    feat: implement settings page layout skeleton

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,38 @@
+import { useHabits } from "../hooks/useHabits";
+
 function Settings() {
+  const { habits } = useHabits();
+
   return (
-    <>
-      <h1>Settings</h1>
-      <p>Settings info will be displayed here.</p>
-    </>
+    <div className="space-y-8">
+      {/* General Settings */}
+      <section>
+        <h2 className="text-2xl font-semibold mb-4">General Settings</h2>
+        <div className="space-y-4">
+          {/* TODO: Dark Mode Toggle */}
+          {/* TODO: Sort Dropdown */}
+          {/* TODO: Motivational Quote Toggle */}
+          {/* TODO: Delete All History Button */}
+          {/* TODO: Reset All Data Button */}
+        </div>
+      </section>
+
+      {/* Habit Settings */}
+      <section>
+        <h2 className="text-2xl font-semibold mb-4">Habit Settings</h2>
+        <div className="space-y-4">
+          {habits.map((habit) => (
+            <div
+              key={habit.id}
+              className="rounded-md border border-gray-700 bg-gray-800 p-4"
+            >
+              <p className="text-white">{habit.name}</p>
+              {/* TODO: Expandable settings */}
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 }
 
