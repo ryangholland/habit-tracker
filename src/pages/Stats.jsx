c3219679f1d923b6e34f9eefa3773ec9a881ff9e
ryangholland <rghcode@gmail.com>
2025-06-12 14:13:39 -0400

    style: change order of Stats page, polish up spacing

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -6,18 +6,19 @@ function Stats() {
   return (
     <div className="min-h-0 flex flex-col">
       <h2 className="text-2xl text-black dark:text-white">Stats</h2>
-      <h3 className="text-xl text-gray-700 dark:text-gray-300 mx-auto">
-        History (Last 4 Months)
-      </h3>
-      <HabitHeatmap />
-      <h3 className="text-xl text-gray-700 dark:text-gray-300 mx-auto mb-2 md:mb-4">
-        Weekly (Last 7 Days)
-      </h3>
-      <WeeklyHabitTable />
-      <h3 className="text-xl text-gray-700 dark:text-gray-300 mx-auto mt-8 md:mt-12">
+      <h3 className="text-xl text-gray-700 dark:text-gray-300 mx-auto mb-2 md:mb-4 mt-2 md:mt-4">
         Summary
       </h3>
       <StatsSummary />
+
+      <h3 className="text-xl text-gray-700 dark:text-gray-300 mx-auto mb-2 md:mb-4 mt-6 md:mt-12">
+        Weekly (Last 7 Days)
+      </h3>
+      <WeeklyHabitTable />
+      <h3 className="text-xl text-gray-700 dark:text-gray-300 mx-auto mt-6 md:mt-12">
+        History (Last 4 Months)
+      </h3>
+      <HabitHeatmap />
     </div>
   );
 }
